680d413435d83c4653b12c7ff9e15d927f7b744b
Yifeng <hyf94244280@gmail.com>
2019-02-03 13:21:15 +0800

    Show local notification
    [#163670083]

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +17,66 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('What to eat'),
-            ),
-            body: Text('What you have eaten today')
-        )
+        home: new HomeApp()
     );
+  }
+}
+
+
+class HomeApp extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<HomeApp> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  initState() {
+    super.initState();
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = new IOSInitializationSettings();
+    var initialSettings = new InitializationSettings(android, iOS);
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(
+        initialSettings, onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) async {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: Text("PayLoad"),
+          content: Text("Payload : $payload"),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    showNotification();
+
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('What to eat'),
+          ),
+          body: new Center(
+              child: RaisedButton(
+                  onPressed: showNotification,
+                  child: Text('Show Notification')
+              )
+          )
+      ),
+    );
+  }
+
+  Future showNotification() async {
+    var android = new AndroidNotificationDetails('channel id', 'channel name', 'channel description');
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(0, 'What did you eat?', 'meat vegi carb nuts', platform);
   }
 }
